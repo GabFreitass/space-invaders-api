0ebbb516eccb9a982930f8af586766cfabcd13c4
Gabriel <bielfreitas@alu.ufc.br>
2024-09-28 06:27:39 -0300

    fix: rooms

--- collections/gameRooms.js
+++ collections/gameRooms.js
@@ -2,12 +2,18 @@ const admin = require("../database/firebase");
 const express = require("express");
 const router = express.Router();
 const gameRules = require("../config/gameRules");
+const { validateRequiredFields } = require("../middlewares/validation");
 
-router.get("/getRoom", async (req, res) => {
-    const { clientId } = req.query; // Obtém o clientId como parâmetro de consulta
-    if (!clientId) {
-        return res.status(400).send("clientId é obrigatório.");
-    }
+router.post("/clearRooms", async (req, res) => {
+    const gameRoomsRef = admin.firestore().collection("gameRooms");
+    const snapshot = await gameRoomsRef.get();
+    snapshot.forEach(async (doc) => {
+        await doc.ref.delete();
+    });
+    res.status(200).send("All game rooms have been cleared.");
+});
+
+router.get("/getAvailableRoom", async (req, res) => {
     const gameRoomsRef = admin.firestore().collection("gameRooms");
 
     // Verifica o número de salas existentes
@@ -24,15 +30,13 @@ router.get("/getRoom", async (req, res) => {
         if (roomData.clientIds.length <= gameRules.maxPlayerPerRoom) {
             roomFound = true;
             roomId = doc.id;
-            roomData.clientIds.push(clientId); // Adiciona o jogador à sala
-            gameRoomsRef.doc(roomId).update(roomData); // Atualiza a sala
         }
     });
 
     // Se não houver salas disponíveis, cria uma nova
     if (!roomFound) {
         const newRoom = {
-            clientIds: [clientId],
+            clientIds: [],
         };
         const docRef = await gameRoomsRef.add(newRoom);
         roomId = docRef.id;
@@ -40,6 +44,32 @@ router.get("/getRoom", async (req, res) => {
 
     res.status(200).json({ roomId });
 });
+
+router.post(
+    "/joinRoom",
+    validateRequiredFields(["roomId", "clientId"]),
+    async (req, res) => {
+        const { roomId, clientId } = req.body; // Obtém o roomId e o clientId como parâmetros do corpo
+        const gameRoomsRef = admin.firestore().collection("gameRooms");
+        const roomDoc = gameRoomsRef.doc(roomId);
+        const roomSnapshot = await roomDoc.get();
+
+        // Verifica se a sala existe
+        if (!roomSnapshot.exists) {
+            return res.status(404).send("Sala não encontrada.");
+        }
+
+        const roomData = roomSnapshot.data();
+        if (roomData.clientIds.length > gameRules.maxPlayerPerRoom) {
+            return res.status(400).send("Sala está cheia.");
+        }
+
+        roomData.clientIds.push(clientId); // Adiciona o jogador à sala
+        await roomDoc.update(roomData); // Atualiza a sala
+
+        res.status(200).send("Jogador adicionado à sala com sucesso.");
+    }
+);
 
 router.delete("/delete", async (req, res) => {
     const { roomId, clientId } = req.query; // Obtém o id da sala e o clientId como parâmetros de consulta
